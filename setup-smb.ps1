d945041df4515169635683f8cc6e36d87fdaf8e3
Mark Wilson <MW0901@proton.me>
2026-01-30 16:44:23 -0800

    Create scheduled task for SMB connection
    Added scheduled task to ensure SMB connection with control node.

--- setup-smb.ps1
+++ setup-smb.ps1
@@ -1,1 +1,11 @@
+$Action = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument '-ExecutionPolicy Bypass -WindowStyle Hidden -File "C:\management\ensure-smb.ps1"'
 
+$Trigger1 = New-ScheduledTaskTrigger -AtStartup
+$Trigger2 = New-ScheduledTaskTrigger -AtLogOn
+$Trigger3 = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5) -RepetitionDuration (New-TimeSpan -Days 3650)
+
+$Principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
+$Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 1)
+
+Register-ScheduledTask -TaskName "Ensure smb connection with control node" -Action $Action -Trigger $Trigger1,$Trigger2,$Trigger3 -Principal $Principal -Settings $Settings
