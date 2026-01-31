ff46d8434837f25e9b326d4e0f56a718d9b80f2e
Mark Wilson <MW0901@proton.me>
2026-01-30 20:08:25 -0800

    Update scheduled task principal and start task
    Change task principal to use the current user and start the task immediately.

--- setup-smb.ps1
+++ setup-smb.ps1
@@ -4,8 +4,10 @@ $Trigger1 = New-ScheduledTaskTrigger -AtStartup
 $Trigger2 = New-ScheduledTaskTrigger -AtLogOn
 $Trigger3 = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5) -RepetitionDuration (New-TimeSpan -Days 3650)
 
-$Principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+$Principal = New-ScheduledTaskPrincipal -UserId "$env:USERDOMAIN\$env:USERNAME" -LogonType Interactive -RunLevel Highest
 
 $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 1)
 
 Register-ScheduledTask -TaskName "Ensure smb connection with control node" -Action $Action -Trigger $Trigger1,$Trigger2,$Trigger3 -Principal $Principal -Settings $Settings
+
+Start-ScheduledTask -TaskName "Ensure smb connection with control node"
